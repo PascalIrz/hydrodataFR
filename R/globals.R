56777368368679badb0a0da2416c8e73b3b7a889
PascalIrz <pascal.irz@gmail.com>
2021-03-24 15:58:41 +0100

    ajout bh_htr_grapher()

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c('.', 'V1'))
+utils::globalVariables(c('.', 'V1', 'QMNA (m3/s) est', 'code_site', 'couleur', 'date_obs', 'debit',
+                         'em', 'libelle_site', 'ma_sh', 'type', 'variable', 'y'))
